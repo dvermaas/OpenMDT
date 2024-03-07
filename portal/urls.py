fbc0ca477d04cb769841c5ab8f98610398241329
dverm <dvermaas@live.com>
2024-03-07 23:21:04 +0100

    frontend

--- portal/urls.py
+++ portal/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:report_id>/", views.detail, name="detail"),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Report
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_report_list = Report.objects.order_by("-created_at")[:5]
+    context = {"latest_report_list": latest_report_list}
+    return render(request, "portal/index.html", context)
 
+
+def detail(request, report_id):
+    report = Report.objects.get(id=report_id)
+    context = {"report": report}
+    return render(request, "portal/detail.html", context)
