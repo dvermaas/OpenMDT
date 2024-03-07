ee2f31d2b5e965c89a5b6f07879d6087ccf58484
dverm <dvermaas@live.com>
2024-03-07 22:09:47 +0100

    models

--- DjangoPolls/urls.py
+++ DjangoPolls/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("portal/", include("portal.urls")),
 ]
---
+++ portal/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Report(models.Model):
+    #label =
+    #status =
+    title = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
