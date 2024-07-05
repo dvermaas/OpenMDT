ecefacc6fe320e7f3afbd83a751ebe0f6627fef4
dverm <dvermaas@live.com>
2024-07-05 21:48:11 +0200

    disable storages for now

--- reports/forms.py
+++ reports/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from .models import Report, Suspect
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout
-from crispy_bootstrap5.bootstrap5 import Switch
+from crispy_forms.layout import Submit
 
 
 class ReportForm(forms.ModelForm):
