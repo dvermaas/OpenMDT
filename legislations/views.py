afb3bdaf1e1697bbc332405dbcd9d0b90763f1c3
dverm <dvermaas@live.com>
2024-08-16 17:30:04 +0200

    legislation contained

--- legislations/views.py
+++ legislations/views.py
@@ -4,6 +4,6 @@ from .models import Legislation
 
 
 def index(request):
-    legislations = Legislation.objects.all()
+    legislations = Legislation.objects.all().order_by("title")
     context = {"legislations": legislations}
     return render(request, "legislations/index.html", context)
