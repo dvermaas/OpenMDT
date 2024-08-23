5cee766e72ff6574fdb385694e3c7e3ef129b0fc
dverm <dvermaas@live.com>
2024-08-24 00:19:41 +0200

    fix broken urlpattern

--- DjangoPolls/urls.py
+++ DjangoPolls/urls.py
@@ -18,7 +18,9 @@ urlpatterns = [
     ),
     path("api/", api.urls),
     path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico")),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
