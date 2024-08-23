59f6670ac5d41f62e49b7f080aae28eee5945833
dverm <dvermaas@live.com>
2024-08-24 00:16:02 +0200

    fix bad indent

--- DjangoPolls/urls.py
+++ DjangoPolls/urls.py
@@ -7,18 +7,18 @@ from django.views.generic.base import TemplateView, RedirectView
 from .api import api
 
 urlpatterns = [
-                  path("", TemplateView.as_view(template_name="home.html"), name="home"),
-                  path("admin/", admin.site.urls),
-                  path("accounts/", include("django.contrib.auth.urls")),
-                  path("__debug__/", include("debug_toolbar.urls")),
-                  path("reports/", include(("reports.urls", "reports"), "reports")),
-                  path("profiles/", include(("profiles.urls", "profiles"), "profiles")),
-                  path(
-                      "legislations/", include(("legislations.urls", "legislations"), "legislations")
-                  ),
-                  path("api/", api.urls),
-                  path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico")),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("reports/", include(("reports.urls", "reports"), "reports")),
+    path("profiles/", include(("profiles.urls", "profiles"), "profiles")),
+    path(
+        "legislations/", include(("legislations.urls", "legislations"), "legislations")
+    ),
+    path("api/", api.urls),
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico")),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
