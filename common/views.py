e47deb94c5b14cb1562b64aa0c89b2fa41bd543e
Dvermaas <31657287+dvermaas@users.noreply.github.com>
2025-01-12 21:48:21 +0100

    Add login_not_required decorator to downloads view

--- common/views.py
+++ common/views.py
@@ -1,3 +1,4 @@
+from allauth.account.internal.decorators import login_not_required
 from django.core.files.storage import default_storage
 from django.http import FileResponse
 from django.shortcuts import render
@@ -8,5 +9,6 @@ from common.models import GenericFile
 def index(request):
     return render(request, "common/ciot.html")
 
+@login_not_required
 def downloads(request, pk):
     return FileResponse(default_storage.open(GenericFile.objects.get(id=pk).file.path))
